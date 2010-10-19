7ba1103fa5eb51f631b8a831b4df4739150de156
Ted Kulp <ted@cmsmadesimple.org>
2010-10-19 05:29:43 -0400

    Gift scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Giftcircle::Application.routes.draw do
+  resources :gifts
+
   devise_for :users
 
   # The priority is based upon order of creation:
---
+++ db/migrate/20101019092741_create_gifts.rb
@@ -0,0 +1,17 @@
+class CreateGifts < ActiveRecord::Migration
+  def self.up
+    create_table :gifts do |t|
+      t.integer :user_id
+      t.string :name
+      t.text :description
+      t.string :url
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :gifts
+  end
+end
